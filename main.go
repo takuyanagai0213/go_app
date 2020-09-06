c2bc603126d995a2304f374e45c599f7dabc1107
TakuyaNagai <nagataku021@outlook.jp>
2020-09-06 20:58:52 +0900

    gormでデータを取得してブラウザに表示した

--- main.go
+++ main.go
@@ -1,41 +1,74 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+
+	"github.com/jinzhu/gorm"
+
 	"log"
 
 	"github.com/gin-gonic/gin"
-
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+type Todo struct {
+	gorm.Model
+	Text   string
+	Status string
+}
 
+func main() {
 	router := gin.Default()
-	data := "test"
 	router.LoadHTMLGlob("views/*.html")
 
+	dbInit()
+
+	todos := dbGetAll()
+
+	fmt.Println(todos)
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(200, "index.html", gin.H{"data": data})
+		ctx.HTML(200, "index.html", gin.H{
+			"todos": todos,
+		})
 	})
 
 	router.Run("localhost:8080")
 }
-func dbConnect() {
-	db, err := sql.Open("mysql", "root@/go_app")
+
+func dbConnect() *gorm.DB {
+	dsn := "root@tcp(localhost:3306)/go_app?parseTime=true"
+
+	db, err := gorm.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return db
+}
+
+func dbInit() {
+	db := dbConnect()
+	db.AutoMigrate(&Todo{})
 	defer db.Close()
+}
 
-	id := 1
-	var name string
-	err = db.QueryRow("SELECT name FROM sample_table WHERE id=?", id).Scan(&name)
-	// defer rows.Close()
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Println(name)
+func dbInsert(text string, status string) {
+	db := dbConnect()
+	todo := Todo{Text: text, Status: status}
+	db.Create(&todo)
+	defer db.Close()
+}
 
+func dbGetAll() []Todo {
+	db := dbConnect()
+	var todos []Todo
+	db.Find(&todos)
+	db.Close()
+	return todos
+}
+func dbGetOne(id int) Todo {
+	db := dbConnect()
+	var todo Todo
+	db.First(&todo, id)
+	db.Close()
+	return todo
 }
